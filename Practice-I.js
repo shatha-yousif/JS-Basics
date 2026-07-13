8e2b6dcca1474e50565c64cdf040e74c65f98627
Shatha Yousef <shathaturkman2021@gmail.com>
2026-07-13 15:19:10 +0300

    practis1

---
+++ Practice-I.js
@@ -0,0 +1,34 @@
+// 1
+let FavoriteAnimal = "cat";
+console.log(FavoriteAnimal);
+
+// 2
+let a=10;
+let b=2;
+console.log(a+b);
+
+// 3
+let age=19;
+console.log("i am " +age +" years old .")
+
+// 4
+let country="palestine";
+country="spain";
+console.log(country);
+
+// 5
+const pi=3.14;
+console.log(pi);
+
+// 6
+let FavoriteColor ="blue";
+console.log(FavoriteColor);
+
+// 7
+let x=5;
+let y=10;
+console.log(x+y);
+
+// 8
+let isSunny=true;
+console.log("It is sunny today.")
